3a076afdd85c10cc735a3ee497f6275ceec8ede4
Hanqing1996 <zhq199622@163.com>
2019-11-28 20:30:22 +0800

    初步优化　popover 代码

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="popover" @click="onClick" style="border:1px solid red">
+    <div class="popover" @click="onClick">
         <div ref="contentWrapper" class="content-wrapper" v-show="visible">
             <slot name="content"></slot>
         </div>
@@ -18,31 +18,31 @@
             }
         },
         methods: {
-            // onClick不是被点击触发的,而是因为内部popover元素被点击,冒泡给popover触发的
+            onOpen(){
+                // setTimeout让"设置document的监听事件"这个动作发生在冒泡结束之后，从而阻止了这次button的点击事件冒泡到document(只阻止了这一次)
+                setTimeout(()=>{
+                    let eventHandler = (event) => {
+
+                        // 只有点击其它位置,才会触发eventHandler("其它位置的定义"是event.target不是文本内容和button)
+                        if(!this.$refs.contentWrapper.contains(event.target)&&!this.$refs.triggerWrapper.contains(event.target)){
+                            this.visible = false
+                            document.removeEventListener('click', eventHandler)
+                        }
+                    }
+
+                    // 设置document的监听事件.这里实际是利用了事件冒泡机制,document内的一切元素的点击事件都会冒泡到document
+                    document.addEventListener('click', eventHandler)
+                },0)
+            },
+
+            // onClick不是被点击触发的,而是因为popover内部元素被点击,冒泡给popover触发的
             onClick(event) {
 
-
-                /**
-                 * 判断冒泡给popover的是哪个元素
-                 * */
-                // 是button触发的,则做进一步操作
+                // 判断冒泡给popover的是哪个元素,若是button触发的,则做进一步操作
                 if (!this.$refs.contentWrapper.contains(event.target)) {
                     this.visible = !this.visible
                     if (this.visible === true) {
-                        // setTimeout让"设置document的监听事件"这个动作发生在冒泡结束之后，从而阻止了这次button的点击事件冒泡到document(只阻止了这一次)
-                        setTimeout(()=>{
-                            let eventHandler = (event) => {
-
-                                if(!this.$refs.contentWrapper.contains(event.target)){
-                                    // console.log('document监听事件开始生效');
-                                    this.visible = false
-                                    document.removeEventListener('click', eventHandler)
-                                }
-                            }
-
-                            // 设置document的监听事件.这里实际是利用了事件冒泡机制,document内的一切元素的点击事件都会冒泡到document
-                            document.addEventListener('click', eventHandler)
-                        },0)
+                        this.onOpen()
                     }
                 }
             }
