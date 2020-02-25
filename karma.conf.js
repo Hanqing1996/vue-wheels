f636dda924a4acb155de9d95aa92601f16a9208f
zhq <zhq199622@163.com>
2020-02-25 10:19:16 +0800

    将 autoWatch 选项设置为 false

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
             dir: './coverage',
             reporters: [{ type: 'lcov', subdir: '.' }, { type: 'text-summary' }],
         },
-        autoWatch: true,
+        autoWatch: false,
 
         browsers: ['ChromeHeadless']
     })
